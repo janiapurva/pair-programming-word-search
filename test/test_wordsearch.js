24a5a9451a14a5e49b87db937c7a4bcc64c8c392
janiapurva <jani.apurva@yahoo.com>
2021-01-13 17:50:29 -0500

    adding final test

--- test/test_wordsearch.js
+++ test/test_wordsearch.js
@@ -1,4 +1,5 @@
 const chai = require('chai');
+const { it } = require('mocha');
 const assert = chai.assert;
 
 const wordSearch = require('../wordsearch.js')
@@ -35,4 +36,31 @@ describe("#wordSearch()", function() {
 
     assert.isTrue(result);
   });
+  
+  it("should return undefined if array is empty", function() {
+    const result = wordSearch([], 'SEINFIELD');
+      
+
+    assert.isFalse(result);
+
+
+  });
+
+  it("should return undefined if empty string", function() {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+      ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+      ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+      ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], '');
+    assert.isUndefined(result);
+
+
+  });
+
 });
--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,45 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
+const transpose = function(matrix) {
+  let newArray = [];
+  let numPlaceholders = 0;
+  
+  for (let i = 0; i < matrix.length; i++) {
+    numPlaceholders = matrix[i].length;
+  }
+  
+  for (let j = 0; j < numPlaceholders; j++) { // building a new empty placeholder set
+    newArray.push([]);
+  }
+  // newArray = [ [], [], [], [], [], [], [], ]   .length = 7
+  // newArray[j]  ^
 
-module.exports = wordSearch
+  for (let j = 0; j < newArray.length; j++) { // looping through newArray
+    for (let x = 0; x < matrix.length; x++) {
+      newArray[j].push(matrix[x][j]); // to each j index of newArray, add matrix[x][j]
+    }
+  }
+  return newArray;
+};
+
+
+
+const wordSearch = (letters, word) => {
+  const verticalFlipped = transpose(letters);
+  const verticalJoin = verticalFlipped.map(ls => ls.join(''));
+  const horizontalJoin = letters.map(ls => ls.join(''));
+  
+  if (letters === [] || word === '') {
+    return undefined;
+  }
+  for (let strings of horizontalJoin) {
+    if (strings.includes(word)) {
+      return true;
+    }
+  }
+  for (let strings2 of verticalJoin) {
+    if (strings2.includes(word)) {
+      return true;
+    }
+  }
+  return false;
+};
+module.exports = wordSearch;
